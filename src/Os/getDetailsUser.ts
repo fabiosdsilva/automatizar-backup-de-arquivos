da23e7dc15e232eccc57c29970fdf2c739149c3f
fabiosdsilva <fabiosds17@gmail.com>
2022-03-06 20:30:19 -0300

    Add javadoc

--- src/Os/getDetailsUser.ts
+++ src/Os/getDetailsUser.ts
@@ -1,7 +1,13 @@
 import os from 'os';
 
+/**
+ * 
+ * @returns Retorna um objeto com informações do hostname, version do OS e o endereço mac.
+ */
+
 export default async function userDetails() {
     const { hostname, version, networkInterfaces } = os;
+
     return {
         hostname: hostname(),
         OS: version(),
