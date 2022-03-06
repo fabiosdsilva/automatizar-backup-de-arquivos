dd21c52274276d3888761416223bb1e641731fd0
fabiosdsilva <fabiosds17@gmail.com>
2022-03-06 19:43:41 -0300

    Funçao modificada para copiar vários arquivos e colar em um diretório

--- src/File/copyFile.ts
+++ src/File/copyFile.ts
@@ -2,15 +2,17 @@ import fs from 'fs';
 
 /**
  * 
- * @param source Caminho do aruivo no qual quer copiar. Ex: "C:\Users\example\Desktop\txt.txt" .
+ * @param source Array contendo o caminho do aruivo no qual quer copiar. Ex: "C:\Users\example\Desktop\txt.txt" . Pode ser mais de um caminho
  * @param dest Caminho no qual o arquivo será enviado. Atenção! O caminha também deverá dizer o nome final do arquivo junto com sua extensão.
  * @returns Retorna uma mensagem de arquivo copiado.
  */
 
-export default async function copyFile(source: string, dest: string) {
+export default async function copyFile(source: string[], dest: string) {
     try {
-        const copy = await fs.copyFileSync(source, dest);
-        return `File copied from ${source} to ${dest}`;
+        for (let i = 0; i < source.length; i++) {
+            await fs.copyFileSync(source[i], dest);
+            console.log(`File copied from ${source[i]} to ${dest}}`);
+        }
     } catch (error: any) {
         return error;
     }
