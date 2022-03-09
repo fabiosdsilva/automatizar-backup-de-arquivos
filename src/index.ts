571d7f2d9679820950bced951a4e83e60b3ce381
fabiosdsilva <fabiosds17@gmail.com>
2022-03-09 11:13:19 -0300

    Funcionando a função para copiar arquivo

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// Iniciar as funções para copiar um arquivo e salvar os detalhes em um json
+import fs from 'fs';
+
+import getDetailsUser from './Os/getDetailsUser';
+import getDetailsFiles from './Os/getDetailsFiles';
+
+import { copyFile } from './File/copyFile';
+
+export default async function index(source: string, dest: string) {
+    try {
+        const user = await getDetailsUser();
+        const fileDetails = await getDetailsFiles(source); // Tenho que passar o caminho do arquivo que quero obter detalhes
+
+        // Processo para copiar um arquivo
+        const file = await copyFile(source, dest) // Tenho que passar um array de caminho para copiar os arquivos e colar em uma única pasta
+
+        // Salvar em arquivo json
+        const obj = { user, fileDetails };
+        const a = await fs.writeFileSync('./Upload/file.json', JSON.stringify(obj));
+        return a;
+    } catch (error) {
+        return error;
+    }
+}
