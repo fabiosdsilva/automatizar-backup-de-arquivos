796d27ce0c810398e3854f3b62baff760d9c9db0
fabiosdsilva <fabiosds17@gmail.com>
2022-03-14 00:58:59 -0300

    Solve function copyFiles

--- src/File/copyFile.ts
+++ src/File/copyFile.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 /**
  * 
  * @param source Array contendo o caminho do aruivo no qual quer copiar. Ex: "C:\Users\example\Desktop\txt.txt" . Pode ser mais de um caminho
- * @param dest Caminho no qual o arquivo será enviado. Atenção! O caminha também deverá dizer o nome final do arquivo junto com sua extensão.
+ * @param dest Caminho no qual o arquivo será enviado.
  * @returns Retorna uma mensagem de arquivo copiado.
  */
 
@@ -16,11 +16,21 @@ async function copyFile(source: string, dest: string) {
 }
 
 async function copyFiles(source: string[], dest: string) {
+    const fileName: string[] = [];
+
+    source.forEach(name => {
+        const lastBar = name.lastIndexOf('/');
+        fileName.push(name.slice(lastBar+1))
+    });
+
     try {
         await source.forEach((file) =>{
-            fs.copyFileSync(file, dest);
-            console.log(`File copied from ${file} to ${dest}}`);
+            fileName.forEach(filename => {
+                fs.copyFileSync(file, dest+filename);
+                console.log(`File copied from ${file} to ${dest}`);
+            })
         });
+
     } catch (error: any) {
         return error;
     }
@@ -29,5 +39,5 @@ async function copyFiles(source: string[], dest: string) {
 export {
     copyFile,
     copyFiles
-};
+}
 
