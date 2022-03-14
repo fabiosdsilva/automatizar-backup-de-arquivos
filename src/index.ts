2f72dec6f60fbb382775b5cef84aeb2a198574a7
fabiosdsilva <fabiosds17@gmail.com>
2022-03-14 01:48:19 -0300

    Organized code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
-import fs from 'fs';
-
 import getDetailsUser from './Os/getDetailsUser';
 import getDetailsFiles from './Os/getDetailsFiles';
 
 import { copyFile } from './File/copyFile';
+import{ writeFile } from './File/writeFile';
 
 export default async function index(source: string, dest: string) {
     try {
         const user = await getDetailsUser();
-        const fileDetails = await getDetailsFiles(source); // Tenho que passar o caminho do arquivo que quero obter detalhes
+        const fileDetails = await getDetailsFiles(source);
+        
+        await writeFile('./src/UploadFile/fileDetails.json', { user, fileDetails });
 
-        // Processo para copiar um arquivo
-        const file = await copyFile(source, dest) // Tenho que passar um array de caminho para copiar os arquivos e colar em uma única pasta
-
-        // Salvar em arquivo json
-        const obj = { user, fileDetails };
-        const fileJson = await fs.writeFileSync('./Upload/file.json', JSON.stringify(obj));
+        await copyFile(source, dest)
+        
     } catch (error) {
         return error;
     }
