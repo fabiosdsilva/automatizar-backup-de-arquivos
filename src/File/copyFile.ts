d4834e5f2d68b6898f7f21ac89efb186d14ca8d2
fabiosdsilva <fabiosds17@gmail.com>
2022-03-14 06:55:33 -0300

    Updated

--- src/File/copyFile.ts
+++ src/File/copyFile.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 /**
  * 
- * @param source Array contendo o caminho do aruivo no qual quer copiar. Ex: "C:\Users\example\Desktop\txt.txt" . Pode ser mais de um caminho
+ * @param source String contendo o caminho do aruivo no qual quer copiar. Ex: "C:\Users\example\Desktop\txt.txt" . Pode ser mais de um caminho
  * @param dest Caminho no qual o arquivo será enviado.
  * @returns Retorna uma mensagem de arquivo copiado.
  */
@@ -16,15 +16,15 @@ async function copyFile(source: string, dest: string) {
     }
 }
 
-async function copyFiles(source: string[], dest: string) {
+async function copyFiles(sourceArray: string[], dest: string) {
     const fileName: string[] = [];
 
-    source.forEach(name => {
+    sourceArray.forEach(name => {
         fileName.push(basename(name));
     });
 
     try {
-        source.forEach( async (file) =>{
+        sourceArray.forEach( async (file) =>{
             await fileName.forEach(filename => {
                 fs.copyFileSync(file, join(dest, filename));
                 console.log(`File copied from ${file} to ${dest}`);
